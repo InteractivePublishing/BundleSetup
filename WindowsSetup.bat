53eea3e2568d087eabb8811cb044556c452de600
James Cook <jamesjcook@noreplay.github.com>
2019-06-25 14:42:31 -0400

    Now uses BaseInstallPath from our setup_vars. Updated to use utils code as expected. Fixed designated version support for atlasviewer package

--- WindowsSetup.bat
+++ WindowsSetup.bat
@@ -1,11 +1,11 @@
 @echo off
-if %PROCESSOR_ARCHITECTURE% EQU AMD64 ( 
+if %PROCESSOR_ARCHITECTURE% EQU AMD64 (
   @REM echo AMD64%PROCESSOR_ARCHITECTURE%
-  set "sevenZ=%~dp0Setup\7z1700-extra\x64\7za.exe"
-) else ( 
+  set "sevenZ=%~dp0Setup\7z1805-extra\x64\7za.exe"
+) else (
   @REM echo is32%PROCESSOR_ARCHITECTURE%
   echo Warning AtlasViewer only compiled for 64-bit architecture.
-  set "sevenZ=%~dp0Setup\7z1700-extra\7za.exe"
+  set "sevenZ=%~dp0Setup\7z1805-extra\7za.exe"
   timeout 30;
   exit /b
 )
@@ -13,7 +13,7 @@ if %PROCESSOR_ARCHITECTURE% EQU AMD64 (
 @REM Get version from index later...
 @REM AppInstaller, LibItemNumber, and LibIndex will probably need to be specified.
 @REM a var_line txt file will probably be best, and this script will look for that.
-call %~dp0\Setup\util\var_line_parser %~dp0\setup_vars.txt
+call %~dp0\Setup\utils\var_line_parser %~dp0\setup_vars.txt
 
 @REM set "LibItemNumber=CIVM-17001"
 @REM set "LibIndex=000Mouse_Brain"
@@ -21,8 +21,10 @@ set "AppBundleName=%WinAppBundleName%"
 set "PROGRAMVERSION=%WinProgramVersion%"
 
 set "astr=-win-amd64_"
+
 set "AppInstaller=%AppBundleName%%astr%%PROGRAMVERSION%.exe"
 
+for /f "delims=" %%F in ('dir /b /s "%~dp0Setup\%AppInstaller%" 2^>nul') do set p=%%F
 
 if not exist %~dp0Setup\%AppInstaller% (
   echo Setup application not found, quiting.
@@ -35,11 +37,13 @@ if not exist %~dp0Setup\%AppInstaller% (
 @REM #    astr"-macosx-amd64";
 
 
-@REM 
+@REM
 
 
-set "BaseInstallPath=C:\InteractivePublishing"
-REM echo "i:%InstallPath%"
+@REM set "BaseInstallPath=C:\InteractivePublishing"
+@REM pushed BaseInstallPath to setup vars.
+@REM currently includes C:\ , maybe we should use %HOMEDRIVE% inside  this script?
+echo "i:%BaseInstallPath%"
 @REM In case there is any madness in the directory given, clobber the vast majortiy of repeated slashes.
 SET "BaseInstallPath=%BaseInstallPath:\\\=\%"
 REM echo "3:%BaseInstallPath%"
@@ -72,14 +76,14 @@ date /T  >> "%LogPath%"
 time /T  >> "%LogPath%"
 
 @REM Extract data
-echo.>%DataUninst% 
-for /f "usebackq delims=|" %%f in (`dir /b "%~dp0*.zip"`) do ( 
+echo.>%DataUninst%
+for /f "usebackq delims=|" %%f in (`dir /b "%~dp0Data\*.zip"`) do (
   echo Extract - %%f
   @REM 7za <command> [<switches>...] <archive_name> [<file_names>...]
   echo %sevenZ% x -o"%DataPath%" -y -aos %~dp0%%f >> "%LogPath%"
   %sevenZ% x -o"%DataPath%" -y -aos %~dp0%%f
   echo %sevenZ% l -o"%DataPath%" -y -aos %~dp0%%f ^>%DataUninst% >> "%LogPath%"
-  %sevenZ% l -o"%DataPath%" -y -aos %~dp0%%f >>%DataUninst% 
+  %sevenZ% l -o"%DataPath%" -y -aos %~dp0%%f >>%DataUninst%
 )
 
 @Rem get data version somehow
@@ -88,11 +92,11 @@ for /f "usebackq delims=|" %%f in (`dir /b "%~dp0*.zip"`) do (
 @REM If that fails, get last dir which starts with v.
 
 dir /b  /O-N /AD "%DataPath%\%LibIndex%\v*" > "%DataPath%\%LibIndex%\vtmp.txt"
-@REM This over complicated line looks for version number folders which match the proper pattern, 
+@REM This over complicated line looks for version number folders which match the proper pattern,
 @REM it gets the last one of those and puts it into the DATAVERSION variable.
 @REM Two temp files are used for this purpose, vtmp.txt, and vrdy.txt both of which are removed when done.
 findstr /R "^v[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$" "%DataPath%\%LibIndex%\vtmp.txt" > "%DataPath%\%LibIndex%\vrdy.txt"  && set /p "DATAVERSION="<"%DataPath%\%LibIndex%\vrdy.txt"  & del "%DataPath%\%LibIndex%\vtmp.txt" & del "%DataPath%\%LibIndex%\vrdy.txt"
-if "%DATAVERSION%"=="" ( 
+if "%DATAVERSION%"=="" (
   echo Data library Not versioned by folder or version failed to set. Only vYYYY-MM-DD type versions supported.
   REM exit /b
 )
@@ -115,19 +119,19 @@ if exist %idx_LibConf% (
     set "var="
     for /f "usebackq" %%A in ("%DataPath%\%LibIndex%\ltmp.txt") do set var=!var! %%A
     echo !var!  ENDECHO >%DataPath%\%LibIndex%\libvars.txt
-    @REM Get the lib vars out of the temp file, use handy dandy varline parser. 
-    call %~dp0\Setup\util\var_line_parser %DataPath%\%LibIndex%\libvars.txt
+    @REM Get the lib vars out of the temp file, use handy dandy varline parser.
+    call %~dp0\Setup\utils\var_line_parser %DataPath%\%LibIndex%\libvars.txt
 
     @REM del  %DataPath%\%LibIndex%\libvars.txt %DataPath%\%LibIndex%\ltmp.txt
     echo Got vo!Version!, and vp!Path!
 
-    IF "!Version!"=="" ( 
+    IF "!Version!"=="" (
         echo Warning, no version in old file.
     ) else (
         if "!Version!" == "%DATAVERSION%" (
             echo Same version, no update needed.
             echo.
-        ) else ( 
+        ) else (
             echo "Data Update detected. new is %DATAVERSION% old is !Version! "
 
             @REM echo "mv \"$idx_LibConf\"  \"${idx_LibConf%.*}.$v.conf\"" >> $LogPath;
@@ -140,15 +144,15 @@ if exist %idx_LibConf% (
     echo  No existing data. Great!
 )
 
-if not exist %idx_LibConf% ( 
+if not exist %idx_LibConf% (
   @REM mkdir "%DataPath%\%LibItemNumber%"
   echo copy "%DataPath%\%LibIndex%\%DATAVERSION%\lib.conf" "%idx_LibConf%" >> "%LogPath%"
   copy "%DataPath%\%LibIndex%\%DATAVERSION%\lib.conf" "%idx_LibConf%"
-  
-  if exist %idx_LibConf% ( 
-    @REM The path echo line adds a newline to the file in case it was missing. 
+
+  if exist %idx_LibConf% (
+    @REM The path echo line adds a newline to the file in case it was missing.
     @REM This line is very fragile, and must be extra careful with extra spaces.
-        
+
     echo @REM insert lib path line "Path=%DATAVERSION%" into %idx_LibConf%>> "%LogPath%"
     findstr /R "^Path=.*$" "%idx_LibConf%" || echo.>>"%idx_LibConf%"&echo Path=%DATAVERSION%>> "%idx_LibConf%"
     findstr /R "^Version=.*$" "%idx_LibConf%" || echo @REM Adding Version line "Version=%DATAVERSION%" into %idx_LibConf%>> "%LogPath%"
@@ -158,11 +162,11 @@ if not exist %idx_LibConf% (
     echo ERROR setting up lib! missing "%idx_LibConf%"!>> "%LogPath%"
   )
 )
-@REM Clean lib of whitesepace then, 
+@REM Clean lib of whitesepace then,
 @REM Get LibName and DATAVERSION from data for use with the program shortcut.
 if exist %idx_LibConf% (
-  call %~dp0\Setup\util\remove_blanks %idx_LibConf%
-  @REM del %idx_LibConf% 
+  call %~dp0\Setup\utils\remove_blanks %idx_LibConf%
+  @REM del %idx_LibConf%
   @REM move %idx_LibConf%.clean %idx_LibConf%
   @REM Get true lib name from file %idx_LibConf%
   @REM Also grab version if its in there.
@@ -170,37 +174,37 @@ if exist %idx_LibConf% (
   echo.>>%DataPath%\%LibIndex%\ltmp.txt
   findstr /R "^Version=.*" "%idx_LibConf%" >> %DataPath%\%LibIndex%\ltmp.txt
 
-  @REM Get the lib vars out of the temp file, use handy dandy varline parser. 
+  @REM Get the lib vars out of the temp file, use handy dandy varline parser.
   setlocal enableDelayedExpansion enableextensions
   set "var="
   for /f "usebackq" %%A in ("%DataPath%\%LibIndex%\ltmp.txt") do set var=!var! %%A
   echo !var!  ENDECHO >%DataPath%\%LibIndex%\libvars.txt
-  
-  @REM echo call %~dp0Setup\util\var_line_parser %DataPath%\%LibIndex%\libvars.txt
-  call %~dp0\Setup\util\var_line_parser %DataPath%\%LibIndex%\libvars.txt
+
+  @REM echo call %~dp0Setup\utils\var_line_parser %DataPath%\%LibIndex%\libvars.txt
+  call %~dp0\Setup\utils\var_line_parser %DataPath%\%LibIndex%\libvars.txt
   REM echo ver=!Version!
   REM echo lib=!LibName!
   del  %DataPath%\%LibIndex%\libvars.txt %DataPath%\%LibIndex%\ltmp.txt
-  
-  IF "!Version!"=="" ( 
+
+  IF "!Version!"=="" (
     ECHO Version is NOT defined in lib.conf
   ) else (
     set DATAVERSION=!Version!
   )
-  IF "!LibName!"=="" ( 
+  IF "!LibName!"=="" (
     set LibName=%LibIndex%
     ECHO LibName is NOT defined in lib.conf
   )
 )
 
-@REM Silent Install app 
+@REM Silent Install app
 set ip=%~dp0Setup\%AppInstaller%
 set InstallPath=%AppPath%\%PROGRAMVERSION%
-if not exist %ip% ( 
+if not exist %ip% (
   echo App installer missing! "%ip%"
 )
 echo %ip% /S /D=%InstallPath% >> "%LogPath%"
-if not exist %InstallPath% ( 
+if not exist %InstallPath% (
   echo "Installing app to %InstallPath%"
   start /b /wait %ip% /S /D=%InstallPath%
 ) else (
@@ -212,14 +216,14 @@ echo %InstallPath%\Uninstall %InstallPath% >> %AppPath%\Uninstall%PROGRAMVERSION
 
 @REM Create shortcut.
 IF "!DATAVERSION!"=="" (
-  echo Version info not set on lib. 
+  echo Version info not set on lib.
   move %DataUninst% %DataPath%\Uninstall_%LibItemNumber%.list
   set "DataUninst=%DataPath%\Uninstall_%LibItemNumber%.list"
-) else ( 
-  IF "!DATAVERSION:~0,1!"=="v" ( 
+) else (
+  IF "!DATAVERSION:~0,1!"=="v" (
     SET "DataVersionString=_d!DATAVERSION:~1!"
   ) else (
-    set "DataVersionString=_d!DATAVERSION!" 
+    set "DataVersionString=_d!DATAVERSION!"
   )
   move %DataUninst% %DataPath%\Uninstall_%LibItemNumber%_!DATAVERSION!.list
   set "DataUninst=%DataPath%\Uninstall_%LibItemNumber%_!DATAVERSION!.list"
@@ -233,11 +237,11 @@ if "%LibItemNumber%"=="" (
 )
 
 set "StartShortcut=%BaseInstallPath%\%LibItemString%%LibName%_a%PROGRAMVERSION%%DataVersionString%"
-echo %~dp0Setup\util\shortcut.bat %InstallPath%\AtlasViewer.exe %InstallPath% InteractivePublishingDataViewer "--ndLibrary %DataPathAlt%/%LibIndex%" >>%LogPath%
+echo %~dp0Setup\utils\shortcut.bat %InstallPath%\AtlasViewer.exe %InstallPath% InteractivePublishingDataViewer "--ndLibrary %DataPathAlt%/%LibIndex%" >>%LogPath%
 if not exist %StartShortcut%.lnk (
   echo "Making lib link to %LibItemString%%LibName%"
-  echo continue in 
-  call %~dp0Setup\util\shortcut.bat %InstallPath%\AtlasViewer.exe %InstallPath% InteractivePublishingDataViewer "--ndLibrary %DataPathAlt%/%LibIndex%"
+  echo continue in
+  call %~dp0Setup\utils\shortcut.bat %InstallPath%\AtlasViewer.exe %InstallPath% InteractivePublishingDataViewer "--ndLibrary %DataPathAlt%/%LibIndex%"
   echo move %InstallPath%\AtlasViewer.lnk %StartShortcut%.lnk >> %LogPath%
   move %InstallPath%\AtlasViewer.lnk %StartShortcut%.lnk
 )
@@ -250,13 +254,13 @@ echo :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo Setup complete.
 echo :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo Log file saved to %LogPath%.
-echo Shortcut to Viewer and data is 
+echo Shortcut to Viewer and data is
 echo %StartShortcut%
 echo.
 echo To uninstall data, delete data folder. WARNING THIS ELIMINATES ALL LIBRARIES.
 echo   %DataPath%
 echo To uninstall application, find the uninstall shortcut from startmenu,
-echo   OR 
+echo   OR
 echo run %AppPath%\Uninstall%PROGRAMVERSION%.bat
 echo :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 timeout 15
