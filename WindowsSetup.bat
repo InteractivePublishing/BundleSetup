1cc52164828cdd342d669c60a0f8da01857e667b
James Cook <jamesjcook@noreplay.github.com>
2020-10-23 14:57:28 -0400

    Finalized path handling to be resliliant to any random windows ugly path

--- WindowsSetup.bat
+++ WindowsSetup.bat
@@ -1,27 +1,33 @@
 @echo off
 setlocal enableDelayedExpansion
 
-for /f "tokens=2" %%a in ("%~df0") do (
-    echo folder path contains space, setup cannot continue, quiting in a few seconds ...
-    timeout /t 8
-    exit /b
-)
-set SetupDir=%~dp0
 
-SET BaseInstallPath=%~dp0
+set "SetupDir=%~dp0"
+@REM 8.3 setup path
+for %%A in ("%SetupDir%") do set "SetupDir=%%~sA"
+
+REM for /f "tokens=2" %%a in ("%~df0") do (
+    REM echo folder path contains space, setup cannot continue, quiting in a few seconds ...
+    REM timeout /t 8
+    REM exit /b
+REM )
+
+SET BaseInstallPath=%SetupDir%
 IF %BaseInstallPath:~-1%==\ SET BaseInstallPath=%BaseInstallPath:~0,-1%
 
 @REM Set logging path.
 SET "LogPath=%BaseInstallPath%\install.log"
 @REM TODO: Prompt user for path to slicer.exe
 @REM set "SlicerPath=c:\Program Files\Slicer\Slicer.exe"
-set "SlicerPath=C:\Program Files\Mozilla Firefox\firefox.exe"
+@REM set "SlicerPath=C:\Program Files\Mozilla Firefox\firefox.exe"
+set "SlicerPath=D:\CIVM_Apps\Slicer\4.11.0-2020-09-25\Slicer.exe"
+@REM 8.3 program path
+for %%A in ("%SlicerPath%") do set "SlicerPath=%%~sA"
 
 set "DataPath=%BaseInstallPath%"
 set "DataUninst=%DataPath%\%LibItemNumber%_tempuninst.list"
 
 for /D %%i in (%DataPath%) do SET "DataVersionString=%%~ni"
-
 
 echo Open Install log  >> "%LogPath%"
 date /T  >> "%LogPath%"
@@ -32,15 +38,24 @@ set "idx_LibConf=%BaseInstallPath%\lib.conf"
 if not exist %idx_LibConf% (
   echo ERROR setting up lib! missing "%idx_LibConf%"!
   echo ERROR setting up lib! missing "%idx_LibConf%"!>> "%LogPath%"
+  timeout 6
   exit /b
 )
 
+@REM   set newar=%Project_Name: =_%
+@REM set "SetupDirArgReady=%SetupDir =\ %
+
+for %%A in (%SetupDir%) do set "SetupDirArgReady=%%~sA"
+
+@REM set PyStart=%DataVersionString%.py
+set PyStart=ndLibrarySupport\Testing\DistStart.py
+
 
 set "StartShortcut=%BaseInstallPath%\StartSlicer_with_%DataVersionString%"
-echo %SetupDir%Components\utils\shortcut.bat "%SlicerPath%" "%BaseInstallPath%" %DataVersionString% "--python-script ^"%SetupDir%%DataVersionString%.py^"" >>%LogPath%
+echo %SetupDir%Components\utils\shortcut.bat "%SlicerPath%" "%BaseInstallPath%" "%DataVersionString%" "--python-script %SetupDirArgReady%%PyStart%" >>%LogPath%
 if not exist "%StartShortcut%.lnk" (
   echo Making lib link to %DataVersionString%
-  call %~dp0Components\utils\shortcut.bat "%SlicerPath%" "%BaseInstallPath%" %DataVersionString% "--python-script ^"%SetupDir%%DataVersionString%.py^""
+  call %~dp0Components\utils\shortcut.bat "%SlicerPath%" "%BaseInstallPath%" "%DataVersionString%" "--python-script %SetupDirArgReady%%PyStart%"
   echo move "%BaseInstallPath%\Slicer.lnk" "%StartShortcut%.lnk" >> %LogPath%
   move "%BaseInstallPath%\Slicer.lnk" "%StartShortcut%.lnk"
 )
